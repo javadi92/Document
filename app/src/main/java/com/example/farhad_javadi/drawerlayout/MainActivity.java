6600a153f7dd14f373bfee441c8e79d2abaf4a4c
javadi92 <farhad.javadi1371@yahoo.com>
2019-03-23 21:34:42 +0430

    Initial commit

---
+++ app/src/main/java/com/example/farhad_javadi/drawerlayout/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.farhad_javadi.drawerlayout;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgDrawerMenu;
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout=(DrawerLayout)findViewById(R.id.drawer);
+
+        imgDrawerMenu=(ImageView) findViewById(R.id.img_menu);
+        imgDrawerMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(drawerLayout.isDrawerOpen(Gravity.RIGHT)){
+                    drawerLayout.closeDrawer(Gravity.RIGHT);
+                }
+                else {
+                    drawerLayout.openDrawer(Gravity.RIGHT);
+                }
+            }
+        });
+
+    }
+}
