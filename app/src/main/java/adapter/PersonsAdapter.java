9078a931d0b4b4d6da5589fd4799ab194416ce13
javadi92 <farhad.javadi1371@yahoo.com>
2019-03-23 22:55:32 +0430

    اضافه کردن آداپتر برای اشخاص

---
+++ app/src/main/java/adapter/PersonsAdapter.java
@@ -0,0 +1,54 @@
+package adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.farhad_javadi.drawerlayout.R;
+import java.util.List;
+import model.Persons;
+
+public class PersonsAdapter extends RecyclerView.Adapter<PersonsAdapter.viewHolder> {
+
+    private List<Persons> list;
+    private Context mContext;
+
+    public PersonsAdapter(Context context,List<Persons> list){
+        this.mContext=context;
+        this.list=list;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view=LayoutInflater.from(mContext).inflate(R.layout.card_person,parent,false);
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        holder.tvPersonName.setText(list.get(position).getPersonName());
+        holder.tvDocumentNumber.setText(list.get(position).getDocumentNumber()+"");
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class viewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvPersonName,tvDocumentNumber;
+        ImageView imgPerson;
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvPersonName=(TextView)itemView.findViewById(R.id.tv_card_person_name);
+            tvDocumentNumber=(TextView)itemView.findViewById(R.id.tv_card_document_number);
+        }
+    }
+}
---
+++ app/src/main/java/model/Persons.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Persons {
+
+    private String personName;
+    private  int documentNumber;
+    private int personImage;
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public int getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(int documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public int getPersonImage() {
+        return personImage;
+    }
+
+    public void setPersonImage(int personImage) {
+        this.personImage = personImage;
+    }
+}
