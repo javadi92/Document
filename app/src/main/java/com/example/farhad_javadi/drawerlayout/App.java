170a25eb954edacff12dbcf4a4345d9b5e985fd9
javadi92 <farhad.javadi1371@yahoo.com>
2019-03-24 17:00:31 +0430

    ساخت کلاس App

---
+++ app/src/main/java/com/example/farhad_javadi/drawerlayout/App.java
@@ -0,0 +1,16 @@
+package com.example.farhad_javadi.drawerlayout;
+
+import android.app.Application;
+
+import database.DBHelper;
+
+public class App extends Application {
+
+    public static DBHelper dbHelper;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        dbHelper=DBHelper.getsInstance(this);
+    }
+}
