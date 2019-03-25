5f81d5f47ce21d53b416df0880e8561a02dd20ff
javadi92 <farhad.javadi1371@yahoo.com>
2019-03-25 14:05:04 +0430

    اضافه کردن alertdialog به دکمه add در activitymain

--- app/src/main/java/com/example/farhad_javadi/drawerlayout/MainActivity.java
+++ app/src/main/java/com/example/farhad_javadi/drawerlayout/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.farhad_javadi.drawerlayout;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
@@ -43,11 +47,23 @@ public class MainActivity extends AppCompatActivity {
         imgPersonsAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(dbHelper.personsInsert("ali","2","1")!=-1){
-                    Toast.makeText(MainActivity.this,"Add button in persons clicked",Toast.LENGTH_LONG).show();
-                }
-                //personsRecyclerView();
-                populatePersonsRecyclerView();
+                Toast.makeText(MainActivity.this,"Add button in persons clicked",Toast.LENGTH_LONG).show();
+                AlertDialog dialog;
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                final EditText editText=new EditText(MainActivity.this);
+                builder.setTitle("انتخاب نام");
+                builder.setPositiveButton("تایید", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(dbHelper.personsInsert(editText.getText().toString(),"0","0")!=-1){
+                            populatePersonsRecyclerView();
+                        }
+                    }
+                    });
+                builder.setView(editText);
+                dialog=builder.create();
+                dialog.show();
+
             }
         });
 
